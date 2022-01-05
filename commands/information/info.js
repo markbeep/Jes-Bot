f2b2f230ecedd559f30a0dc25ba8693399fdabdc
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-01-05 22:50:39 +0100

    had to update invite link

--- commands/information/info.js
+++ commands/information/info.js
@@ -8,7 +8,7 @@ info.shortDescription = "Displays bot information & the bot invite link";
 info.command = async function (msg, args, client) {
     desc = `**Uptime:** \`${Math.round(client.uptime / 36e4) / 10}\` hours\n` // rounded to .1
         + `**Node.js Version:** \`${process.version}\`\n`
-        + `**Invite:** [Invite me!](https://discord.com/api/oauth2/authorize?client_id=920348995933589554&permissions=2147485696&scope=bot)`
+        + `**Invite:** [Invite me!](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=2147485696&scope=bot%20applications.commands)`
     await msg.channel.send({ embeds: [basic("Info", desc)] });
 }
 
