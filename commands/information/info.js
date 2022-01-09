24b30b2ee5e96dec06e5bb1159ccc82fcef7d6b2
Mark Csurgay <mcsurgay@student.ethz.ch>
2022-01-09 19:59:25 +0100

    info show quotes count for all

--- commands/information/info.js
+++ commands/information/info.js
@@ -7,13 +7,9 @@ info.description = "Displays a variaty of statistics about the bot.";
 info.shortDescription = "Displays bot information & the bot invite link";
 
 info.command = async function (msg, args, client) {
-    const quoteCount = await QuoteModel.count({
-        where: {
-            guildId: msg.guild.id
-        }
-    });
+    const quoteCount = await QuoteModel.count();
     desc = `**Uptime:** \`${Math.round(client.uptime / 36e4) / 10}\` hours\n` // rounded to .1
-        + `**# of Quotes:** \`${quoteCount}\`\n`
+        + `**# of Quotes on all servers:** \`${quoteCount}\`\n`
         + `**# of Servers: \`${client.guilds.cache.size}\`**\n`
         + `**Node.js Version:** \`${process.version}\`\n`
         + `**Invite:** [Invite me!](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=2147485696&scope=bot%20applications.commands)`
